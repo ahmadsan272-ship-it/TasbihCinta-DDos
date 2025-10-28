55fa566a092bc00dd7b792648d13b9727241628a
Zan-0xy <ahmadsan272@gmail.com>
2025-10-28 19:13:15 +0700

    Update tasbih-cinta.py

--- tasbih-cinta.py
+++ tasbih-cinta.py
@@ -10,7 +10,22 @@ import logging
 
 def warna():
 
-
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    UNDERLINE = '\033[4m'
+    PURPLE = '\033[97m'
+    BOLD    = "\033[1m"
+    BLACK   = "\033[30m"
+    RED     = "\033[31m"
+    GREEN   = "\033[32m"
+    YELLOW  = "\033[33m"
+    BLUE    = "\033[34m"
+    MAGENTA = "\033[35m"
+    CYAN    = "\033[36m"
+    WHITE   = "\033[37m"
 
 \033[2mgray = abu-abu
 
