f3a5a651758d255932ecc7e7a8ec5ec57aca1acb
Zan-0xy <ahmadsan272@gmail.com>
2025-10-28 14:16:55 +0700

    Update tasbih-cinta.py

--- tasbih-cinta.py
+++ tasbih-cinta.py
@@ -18,6 +18,21 @@ os.system("clear")
 print("")
 
 
+
+
+
+while attemps < 100:
+    username = input("\033[32mEnter your username: \033[0m")
+    password = input("\033[31mEnter your password: \033[0m")
+
+    if username == 'bp4' and password == 'bp4':
+        print("\033[32m⟩⟩ Hai...! Welcome to zona attack BLACKPHANTER \033[0m")
+        break
+    else:
+        print('Incorrect credentials. Check if you have Caps lock on and try again.')
+        attemps += 1
+        continue
+
 if len(sys.argv) < 4:
     print "ddos4"
     sys.exit("Usage: python "+sys.argv[0]+" <ip> <port> <size>")
